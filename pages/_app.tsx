7271cc6eda7171363880a8d16af54c61f0a4ac8c
Philipp Muens <philipp@muens.io>
2022-05-30 09:18:08 +0200

    Setup Next SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import { DefaultSeo } from 'next-seo'
 import type { AppProps } from 'next/app'
 
 import Head from '../components/Head'
@@ -7,6 +8,11 @@ import Layout from '../components/Layout'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <DefaultSeo
+        title="Placeholder"
+        description="Lorem ipsum dolor sit amet."
+        titleTemplate="%s - Placeholder"
+      />
       <Head />
       <Layout>
         <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
 import type { NextPage } from 'next'
+import { NextSeo } from 'next-seo'
 
 const Home: NextPage = () => {
   return (
-    <section>
-      <h1>Placeholder</h1>
-      <p>Lorem ipsum dolor sit amet.</p>
-    </section>
+    <>
+      <NextSeo title="Home" description="Lorem ipsum dolor sit amet." />
+      <section>
+        <h1>Placeholder</h1>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </section>
+    </>
   )
 }
 
