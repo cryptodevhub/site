5ba05cb0692a1e0dbad370b54e07fc09564ef6af
Philipp Muens <philipp@muens.io>
2022-06-06 10:25:00 +0200

    Add Layout component styles

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,10 +4,10 @@ import Footer from './Footer'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Header />
       <Main>{children}</Main>
       <Footer />
-    </>
+    </div>
   )
 }
