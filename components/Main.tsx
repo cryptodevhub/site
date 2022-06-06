1c53fe7a6f42df136460ac84fd0022f65ee1572b
Philipp Muens <philipp@muens.io>
2022-06-06 10:29:26 +0200

    Add Main component styles

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,3 +1,3 @@
 export default function Main({ children }: { children: React.ReactNode }) {
-  return <main>{children}</main>
+  return <main className="max-w-7xl mx-auto p-4 flex-1">{children}</main>
 }
