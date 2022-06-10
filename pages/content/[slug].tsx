d5e8dac648a7eda7e6288ab0069534997072221a
Philipp Muens <philipp@muens.io>
2022-06-10 09:16:50 +0200

    Finalize content show page

--- pages/content/[slug].tsx
+++ pages/content/[slug].tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { NextSeo } from 'next-seo'
 import { GetStaticPaths, GetStaticProps } from 'next'
 
@@ -8,31 +9,43 @@ export default function Show({ content }: { content: Content }) {
   return (
     <>
       <NextSeo title={content.title} description={content.description} />
-      <section>
-        <h1>
-          <a href={content.url} target="_blank" rel="noreferrer">
-            {content.title}
-          </a>
-        </h1>
-        <div dangerouslySetInnerHTML={{ __html: content.body }} />
-        <ul>
-          <ul>
-            {content.authors.map((author) => (
-              <li key={author}>{author}</li>
+      <article>
+        <div className="text-center mb-8">
+          <h1 className="font-bold text-4xl">
+            <a href={content.url} target="_blank" rel="noreferrer">
+              {content.title}
+            </a>
+          </h1>
+        </div>
+        <div className="prose max-w-none" dangerouslySetInnerHTML={{ __html: content.body }} />
+        <div className="flex items-center justify-between my-6">
+          <ul className="flex">
+            {content.authors.map((author, index) => (
+              <li key={author}>{(index ? ', ' : '') + author}</li>
             ))}
           </ul>
-        </ul>
-        <ul>
+          <p>
+            <Date date={content.updated} />
+          </p>
+        </div>
+        <div className="flex items-center justify-between">
           <ul>
             {content.tags.map((tag) => (
-              <li key={tag}>{tag}</li>
+              <li
+                key={tag}
+                className="mr-2 badge badge-outline hover:badge-primary hover:badge-outline"
+              >
+                <Link href={`/tags/${tag}`}>
+                  <a>{tag}</a>
+                </Link>
+              </li>
             ))}
           </ul>
-        </ul>
-        <p>
-          <Date date={content.updated} />
-        </p>
-      </section>
+          <a href={content.url} target="_blank" className="btn btn-primary" rel="noreferrer">
+            Go to Website
+          </a>
+        </div>
+      </article>
     </>
   )
 }
